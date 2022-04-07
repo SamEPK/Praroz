6c3db490daff438ea43d6adab4ffe8f83738ecca
Samalairbien <74256932+Samalairbien@users.noreply.github.com>
2022-04-07 19:39:54 +0200

    ouai mdr

---
+++ js/app.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("chargé");
+
+  let exprmail =  /^[a-zA-Z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{2,4}$/;
+  let exprmdp ="sam";
+
+ warning = document.querySelector(".warning");
+ let email = document.getElementsByTagName("input")[1].value;
+ let password = document.getElementsByTagName("input")[2].value;
+ let btn = document.getElementsByTagName("input")[3];
+  btn.addEventListener("click", e => {
+      e.preventDefault();
+      send();
+  });
+
+         let send = () => {
+      email = document.getElementsByTagName("input")[1].value;
+      password = document.getElementsByTagName("input")[2].value;
+      btn = document.getElementsByTagName("input")[3];
+
+      if (email.match(exprmail) && password.match(exprmdp)) {
+          warning.classList.add("success");
+          warning.innerText = "Connexion réussie";
+          localStorage.setItem("user", email);
+          sessionStorage.setItem("user", "sessionid");
+          alert("Bienvenue "+ password);
+          console.log("Connexion réussie")
+        } else if (email.match(exprmail)) {
+          warning.innerText = "Mot de passe invalide";
+          warning.classList.remove("success");
+          console.log("Mot de passe invalide");
+      } else if (password.match(exprmdp)) {
+          warning.innerText = "Mail invalide";
+          warning.classList.remove("success");
+          console.log("Mail invalide")
+      } else {
+          warning.innerText = "Mail ou mot de passe incorrecte";
+          warning.classList.remove("success");
+          console.log("Mail ou mot de passe incorrecte");
+      }
+  };
+});
---
+++ readme.md
@@ -0,0 +1,25 @@
+    ## HTML
+
+    Absence des balises ``<meta>`` dans le ``<head>`` <br>
+    Titre Praroz dans une ``div>h2``, non utilisation d'un ``<header> <h1>``<br>
+    Utilisation de ``<div>``, balises non sémantiques <br>
+    ``<button>`` dans un ``<a href="#">``, et inversement ``<a href="#">`` dans un ``<button>``<br>
+    Balise ``<main>`` remplacé par une ``<div class="main">``<br>
+    ``<h1>`` dans une ``<div>`` au lieu de ``<h2>`` (sans compter l'utilisation de ``div``)<br>
+    Balise ``<a href="#">`` en dehors d'un ``ul>li``<br>
+    Fermetures inutiles de balise ``<div>`` <br>
+    Balise ``<form>`` non utilisé pour le login <br>
+    Code non sémantique.
+
+    ---
+
+    ## CSS
+
+    Utilisation de ``px`` à la place de ``rem``<br>
+    Non responsive <br>
+    Absence de reset <br>
+
+    ## JS
+
+    Absence du ``DOM`` qui permet le chargement du JS en premier sur la page <br> 
+    Il vaut mieux utiliser ``let`` que ``var``
